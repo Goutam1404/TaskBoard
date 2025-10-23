a602eb3096da30857adad7929bba74a444ae46b1
Goutam1404 <goutamkumar9093@gmail.com>
2025-10-23 22:12:29 +0530

    methods for updating and getting the todos

--- backend/src/controllers/Todo.controller.js
+++ backend/src/controllers/Todo.controller.js
@@ -6,8 +6,8 @@ const createTodo = async (req, res) => {
 
     if (!title) {
       return res.status(400).json({
-        message: "Title is required",
         success: false,
+        message: "Title is required",
       });
     }
     const todo = new Todo({
@@ -17,15 +17,60 @@ const createTodo = async (req, res) => {
     console.log("Creating a task");
     await todo.save();
     return res.status(200).json({
-      message: "Todo created successfully",
       success: true,
+      message: "Todo created successfully",
       todo,
     });
   } catch (error) {
     return res.status(500).json({
-      message: "Error in creating the todo",
       success: false,
+      message: "Error in creating the todo",
       error,
     });
   }
 };
+
+const getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    return res.status(200).json({
+      success: true,
+      message: "All todos fetched",
+      todos,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: `Error in fetching todo`,
+      error,
+    });
+  }
+};
+
+const updateTodos = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, tasks } = req.body;
+    const todo = await Todo.findOne({ _id: id });
+    if (!todo) {
+      return res.status(404).json({
+        message: "can't found the Todo",
+        success: false,
+      });
+    }
+    todo.title = title || todo.title;
+
+    await todo.save();
+    return res.status(200).json({
+      success: true,
+      message: "Notes updates successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error in updating the todo",
+    });
+  }
+};
+
+export { createTodo, getTodos, updateTodos };
