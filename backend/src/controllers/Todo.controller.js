c7f78e7f8bca7fdb41d243ac19c41c144643b6c1
Goutam1404 <goutamkumar9093@gmail.com>
2025-10-24 00:45:48 +0530

    completed the crud for todo list

--- backend/src/controllers/Todo.controller.js
+++ backend/src/controllers/Todo.controller.js
@@ -47,23 +47,57 @@ const getTodos = async (req, res) => {
   }
 };
 
+const getTodoById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404 ).json({
+        success: false,
+        message: "Unable to get the todo",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Todo found",
+      todo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Some error. Unable to get the todo",
+      error,
+    });
+  }
+};
+
 const updateTodos = async (req, res) => {
   try {
     const { id } = req.params;
     const { title, tasks } = req.body;
-    const todo = await Todo.findOne({ _id: id });
-    if (!todo) {
+    const updates = {};
+    if (title !== undefined) {
+      updates.title = title;
+    }
+    if (tasks !== undefined) {
+      updates.tasks = tasks;
+    }
+    // MongoDB will only '$set' the fields that exist in our 'updates' object.
+    // Any field NOT in 'updates' will be completely ignored and left as is in the database.
+    const updatedTodo = await Todo.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+    if (!updatedTodo) {
       return res.status(404).json({
         message: "can't found the Todo",
         success: false,
       });
     }
-    todo.title = title || todo.title;
 
-    await todo.save();
     return res.status(200).json({
       success: true,
       message: "Notes updates successfully",
+      updatedTodo,
     });
   } catch (error) {
     return res.status(500).json({
@@ -73,4 +107,29 @@ const updateTodos = async (req, res) => {
   }
 };
 
-export { createTodo, getTodos, updateTodos };
+const deleteTodo = async (req, res) => {
+    try {
+      const {id}=req.params;
+  
+      const deleted= await Todo.findByIdAndDelete(id);
+      if(!deleted){
+        return res.status(400).json({
+          success:false,
+          message:"Some error in deleting the Todo List"
+        })
+      }
+      return res.status(200).json({
+        success:true,
+        message:"Successfully deleted the todo list",
+        deleteTodo
+      })
+    } catch (error) {
+       return res.status(500).json({
+         success: false,
+         message: "Server error in deleting the todo list",
+         error,
+       });
+    }
+};
+
+export { createTodo, getTodos, updateTodos, getTodoById, deleteTodo };
