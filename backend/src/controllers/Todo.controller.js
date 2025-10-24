946dfee66e927e35a1ab23e9f102c9b4737958f7
Goutam1404 <goutamkumar9093@gmail.com>
2025-10-24 21:39:12 +0530

    Completed the controller for todos

--- backend/src/controllers/Todo.controller.js
+++ backend/src/controllers/Todo.controller.js
@@ -52,7 +52,7 @@ const getTodoById = async (req, res) => {
   try {
     const todo = await Todo.findById(id);
     if (!todo) {
-      return res.status(404 ).json({
+      return res.status(404).json({
         success: false,
         message: "Unable to get the todo",
       });
@@ -108,28 +108,141 @@ const updateTodos = async (req, res) => {
 };
 
 const deleteTodo = async (req, res) => {
-    try {
-      const {id}=req.params;
-  
-      const deleted= await Todo.findByIdAndDelete(id);
-      if(!deleted){
-        return res.status(400).json({
-          success:false,
-          message:"Some error in deleting the Todo List"
-        })
-      }
-      return res.status(200).json({
-        success:true,
-        message:"Successfully deleted the todo list",
-        deleteTodo
-      })
-    } catch (error) {
-       return res.status(500).json({
-         success: false,
-         message: "Server error in deleting the todo list",
-         error,
-       });
+  try {
+    const { id } = req.params;
+
+    const deleted = await Todo.findByIdAndDelete(id);
+    if (!deleted) {
+      return res.status(400).json({
+        success: false,
+        message: "Some error in deleting the Todo List",
+      });
     }
+    return res.status(200).json({
+      success: true,
+      message: "Successfully deleted the todo list",
+      deleteTodo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error in deleting the todo list",
+      error,
+    });
+  }
 };
 
-export { createTodo, getTodos, updateTodos, getTodoById, deleteTodo };
+//controller for tasks
+const addTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { task } = req.body; //in model it is -> taskName
+    if (!id || !task) {
+      return res.status(400).json({
+        success: false,
+        message: "task is required as input",
+      });
+    }
+
+    //updating the task
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      {
+        $push: { tasks: { task } },
+      },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return res.status(404).json({
+        success: false,
+        message: "Error in updating the task",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Task added successfully",
+      updatedTodo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error in adding the task",
+      error,
+    });
+  }
+};
+
+const updatedTask = async (req, res) => {
+  try {
+    const { id, taskId } = req.parmas;
+    const { task, isCompleted } = req.body;
+    if (!id || !taskId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const updateTodo = await Todo.findById(id);
+    const updateTask = updateTodo.tasks.id(taskId);
+    if (!updateTask || !updateTodo) {
+      return res.status(404).json({
+        success: false,
+        message: "Can't find the task",
+      });
+    }
+    if (task !== undefined) updateTask.taskName = task;
+    if (isCompleted !== undefined) updateTask.isCompleted = isCompleted;
+
+    await Todo.save();
+    return res.status(200).json({
+      success: true,
+      message: "Successfully updated the task",
+      Todo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error in updating the task",
+      error,
+    });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id, taskId } = req.parmas;
+    const todo = await Todo.findByIdAndUpdate(
+      id,
+      { $pull: { tasks: { _id: taskId } } },
+      { new: true }
+    );
+
+    if (!todo) {
+      return res.status(400).json({
+        success: false,
+        message: "Something went wrong in deleting",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Task deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error in deleting task",
+    });
+  }
+};
+
+export {
+  createTodo,
+  getTodos,
+  updateTodos,
+  getTodoById,
+  deleteTodo,
+  addTask,
+  updatedTask,
+  deleteTask,
+};
